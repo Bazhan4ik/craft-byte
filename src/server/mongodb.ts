3a7d685f3ea92dc768a295a416969d15b10e75e5
Bazhan Nagnybida <bazhantt@gmail.com>
2023-07-05 18:01:12 -0400

    hide mongodb url

--- src/server/mongodb.ts
+++ src/server/mongodb.ts
@@ -1,5 +1,5 @@
 import { MongoClient } from 'mongodb';
 
-const client = new MongoClient('mongodb+srv://bazhantt:hUqFgk0EiWd3NIjz@cluster0.syt6c4t.mongodb.net/?retryWrites=true&w=majority');
+const client = new MongoClient(process.env["MONGO_URL"] as string);
 
 export default client;
